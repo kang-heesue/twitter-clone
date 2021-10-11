6a6553895361b2c8cdb5cdd23e97679905f67337
kang-heesue <heesue.dev@gmail.com>
2021-10-11 17:36:27 +0900

    feat: authentication

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import Nav from 'components/Nav';
+import Auth from 'routes/Auth';
+import Home from 'routes/Home';
+import Profile from 'routes/Profile';
 
-function Router() {
-  const [isLogin, setIsLogin] = useState(false);
+function Router({ isSignIn }) {
   return (
     <HashRouter>
+      {isSignIn && <Nav />}
       <Switch>
-        {isLogin ? (
+        {isSignIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
         )}
       </Switch>
     </HashRouter>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { authService } from 'fbase';
 
 function Profile() {
-  return <span>Profile</span>;
+  const history = useHistory();
+
+  const onLogoutClick = () => {
+    authService.signOut();
+    history.push('/');
+  };
+
+  return (
+    <>
+      <button onClick={onLogoutClick}>Logout</button>
+    </>
+  );
 }
 
 export default Profile;
